a9b1e39c5a508281ebcc3fc40a115db3cc3e2ec6
face-to-face224 <thworkspace8@gmail.com>
2025-01-17 20:44:09 +0800

    Update addon.py

--- addon.py
+++ addon.py
@@ -5,9 +5,9 @@ import xbmcvfs
 
 class BootAtStart(xbmcaddon.Addon):
     def __init__(self):
-        self.addon = xbmcaddon.Addon()
-        self.addon_name = self.addon.getAddonInfo('name')
-        self.settings = self.addon.getSettings()
+        super().__init__()
+        self.addon_name = self.getAddonInfo('name')
+        self.settings = self.getSettings()
         self.selected_addon = self.settings.getSetting('selected_addon')
 
     def run(self):
@@ -15,15 +15,18 @@ class BootAtStart(xbmcaddon.Addon):
             xbmc.executebuiltin(f'RunAddon({self.selected_addon})')
 
     def get_addons(self):
+        """Returns list of tuples containing addon ID and name."""
         addons = []
-        for dir in xbmcvfs.listdir('special://home/addons')[1]:
+        dirs = xbmcvfs.listdir('special://home/addons')[1]
+        for dir in dirs:
             if dir.startswith('plugin.') or dir.startswith('script.'):
-                addons.append((dir, xbmcaddon.Addon(dir).getAddonInfo('name')))
+                addon = xbmcaddon.Addon(dir)
+                addons.append((dir, addon.getAddonInfo('name')))
         return addons
 
 def main():
     boot_at_start = BootAtStart()
-    if boot_at_start.settings.getSetting('selected_addon') == '':
+    if not boot_at_start.selected_addon:
         dialog = xbmcgui.Dialog()
         addons = boot_at_start.get_addons()
         addon_list = [addon[1] for addon in addons]
